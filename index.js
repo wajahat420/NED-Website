2a366edf9c66c544c379f0c0948352133665e8c2
wajahat420 <shayanmustafa11@gmail.com>
2020-10-16 18:32:27 +0000

    backend connected

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express()
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser")
-const path = require('path');
-const router = express.Router()
+//const mongoose = require("mongoose");
+//const bodyParser = require("body-parser")
+//const path = require('path');
+//const router = express.Router()
 // const index = require("./Routes/index")
 
 // const mongoURI = "mongodb://localhost/E-dealers"
@@ -12,15 +12,17 @@ const router = express.Router()
 // app.use(bodyParser.urlencoded({extended : false}))
 // app.use(bodyParser.json())
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+//app.use(bodyParser.json({limit: '50mb'}));
+//app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/assets'));
 
-router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
-    //__dirname : It will resolve to your project folder.
-  });
+
+app.get('/',function(req,res) {
+  res.sendFile('/home/wajahat/Desktop/NED-Website/index.html');
+});
 // app.use("/",index)
-app.use('/', router);
+//app.use('/', router);
 const port = 5000
 
 app.listen(port, () => console.log(`server running on port ${port}`))
